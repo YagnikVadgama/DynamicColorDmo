3a20915fab9081eda12d7c760cac89f738f0e21f
Yagnik Vadgama <yagnik.v@yudiz.com>
2022-03-16 18:26:21 +0530

    - minor modifications

--- app/src/main/java/com/example/dynamiccolordmo/DynamicColor.kt
+++ app/src/main/java/com/example/dynamiccolordmo/DynamicColor.kt
@@ -6,7 +6,7 @@ import com.google.android.material.color.DynamicColors
 class DynamicColor: Application() {
     override fun onCreate() {
         super.onCreate()
-      //  DynamicColors.applyToActivitiesIfAvailable(this, R.style.AppTheme_Overlay)
+        //DynamicColors.applyToActivitiesIfAvailable(this, R.style.Theme_DynamicColorDmo)
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 }
